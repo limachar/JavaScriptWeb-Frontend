78f59245e6aa5ba7b3bd0158faaac0ab127eb122
linn.mann@gmail.com <linn.mann@gmail.com>
2022-02-11 11:12:12 +0100

    prepared frontend for heroku

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+var favicon = require('serve-favicon')
+var path = require('path')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+app.use('/healthcheck', require('./routes/healthcheck.routes'));
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + "/public/index.html")})
+
+app.listen(PORT, function(){
+    console.log(`SERVER STARTED ON PORT ${PORT}`)})
