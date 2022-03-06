4c4989c25af20c2771e8dc86f06afb51719f1f4b
linn.mann@gmail.com <linn.mann@gmail.com>
2022-03-06 10:34:01 +0100

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,12 @@
-var user
+let user = sessionStorage.getItem("user")
+console.log(user)
+if(user==null){
+    console.log("no user")
+    $.get('https://lima-backend.herokuapp.com/auth', function(response){
+       user = response.user
+       $(".header-box").append('<p>'+user+'</p>')
+    })
+}
 
 $(function(){
     $("#subbtn").on('click', function() {
@@ -26,23 +34,23 @@ $(function(){
         var message = $('.message-text').html();
         console.log(message)
         var date = new Date();
-        $(".chat-box").append("<div id='message'><div id='message-box'>"/*+date.toDateString(),user, ": ", */+message+"</div></div>");
+        
         $.ajax({
             type: "POST",
-            url: "http://localhost:3000/messages",
-            data: message,
+            url: "https://lima-backend.herokuapp.com/messages",
+            data: {"message":message},
             success: function(result) {
-                alert('ok');
                 console.log('success')
             },
             error: function(result) {
                 alert('error');
             }
         })
+        $.get('https://lima-backend.herokuapp.com/message', function(response){
+            console.log(response)
+            let lastMessage = response[response.length-1]
+            console.log(lastMessage)
+            $(".chat-box").append("<div id='message'>"+user+':'+"<div id='message-box'>"/*+date.toDateString(),user, ": ", */+lastMessage.message+"</div></div>")
+    })
     });
 });
-var chat = {
-    responses: [
-        'Hello', 'What is your name', 'Nice to meet you'
-    ]
-}
